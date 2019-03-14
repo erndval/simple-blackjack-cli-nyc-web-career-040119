fbe74bc66162f009fc9133539ed847cb0b853b15
Ernie Val <erndval@gmail.com>
2019-03-14 21:38:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,15 +23,26 @@ def end_game(card_total)
 end
 
 def initial_round
-  # code #initial_round here
+first_draw = deal_card
+second_draw = deal_card
+card_total = first_draw + second_draw
+display_card_total(card_total)
+return card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  get_user_input
+  if user_input == h
+    card_total = card_total + deal_card
+  elsif user_input == s
+  display_card_total
+else invalid_command
 end
 
 def invalid_command
-  # code invalid_command here
+ puts "Please enter a valid command."
+ prompt_user
 end
 
 #####################################################
