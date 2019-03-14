76f80eeb05f304fab595e2bd04e0e3b2e4a4f80e
Ernie Val <erndval@gmail.com>
2019-03-14 21:45:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,9 +33,9 @@ end
 def hit?(card_total)
   prompt_user
   user_input = get_user_input
-  if user_input == h
+  if user_input == "h"
     card_total = card_total + deal_card
-  elsif user_input == s
+  elsif user_input == "s"
   display_card_total
 else invalid_command
 end
